6012db2102acb391db730fd6acfe2e8425b655b5
yamakou1125 <yamakou.93.11.25@gmail.com>
2021-09-07 07:02:14 +0000

    [Add] 投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,13 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
   end
 
   def show
@@ -7,4 +15,10 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+ private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
