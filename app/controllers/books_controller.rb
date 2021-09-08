d4854a2ec38fc37b0d27b92643f3ce36d84d3efb
yamakou1125 <yamakou.93.11.25@gmail.com>
2021-09-08 15:59:57 +0000

    [add] バリデーション（投稿時）

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,10 +5,11 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    if book.save
-    redirect_to book_path(book), notice: "Book was successfully created."
+    @book = Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book), notice: "Book was successfully created."
     else
+      @books = Book.all
      render :index
     end
   end
